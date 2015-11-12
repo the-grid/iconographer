4067a366afb8f032696d12a220236b209803a32d
Øyvind Kolås <pippin@gimp.org>
2015-11-12 23:01:06 +0100

    make within first third also a more homogenous property

--- iconographer.c
+++ iconographer.c
@@ -261,11 +261,10 @@ float score_frame (FrameInfo *info, int frame_no)
                                 info->rgb_square_diff[2] / 255.0) * 3;
   float after_first_40_sec    = frame_no / frame_rate > 40.0 ? 1.0 : 0.04;
   float after_first_12_sec    = frame_no / frame_rate > 12.0 ? 1.0 : 0.04;
-  float within_first_third    = frame_no < total_frames / 3 ? 1 : 0;
+  float within_first_third    = frame_no < total_frames / 3 ? 1 : 0.6;
 
   sum_score = rgb_histogram_count;
-  sum_score += within_first_third * 0.33;
-
+  sum_score *= within_first_third * 0.33;
   sum_score *= after_first_40_sec  * 0.33;
   sum_score *= after_first_12_sec  * 0.33;
 
